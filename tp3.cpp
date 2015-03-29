059fa3e0be0a98602127368bbdb6df5e3f685d0c
jorandria <j.randrianandrasana@gmail.com>
2015-03-29 17:37:45 +0200

    [Eclairage direct]  + Méthode de base OK: Monte-Carlo à implémenter

--- tp3.cpp
+++ tp3.cpp
@@ -6,11 +6,11 @@
 
 #include <vector>
 #include <string>
-#include <stdio.h>
+#include <cstdio>
 
 gk::Mesh* _mesh;
 std::vector<gk::Vec4> _triangleColors;
-std::vector<gk::Triangle> _lights;
+std::vector<uint> _lights;
 
 //! Resources
 gk::Mesh* loadMesh(const std::string& filename)
@@ -29,22 +29,22 @@ gk::Mesh* loadMesh(const std::string& filename)
 
 void loadScene()
 {
-  int i;
-  int triangleCount;
+  uint i;
+  uint triangleCount;
 
   float colorDelta;
 
   _mesh = loadMesh("synthese_tp3/scenes/geometry.obj");
 
-  triangleCount = _mesh->triangleCount();
+  triangleCount = (uint)_mesh->triangleCount();
   colorDelta = 0.25f / triangleCount;
 
   for (i = 0; i < triangleCount; ++i)
   {
     _triangleColors.push_back(gk::Vec4(0.5f + (i * colorDelta), 0.5f + (i * colorDelta), 0, 1));
 
-    if (!(_mesh->triangleMaterial((uint)i).emission == gk::Vec4(0, 0, 0, 0)))
-      _lights.push_back(_mesh->triangle((uint)i));
+    if (!(_mesh->triangleMaterial(i).emission == gk::Vec4(0, 0, 0, 0)))
+      _lights.push_back(i);
   }
 }
 void deleteScene()
@@ -53,52 +53,126 @@ void deleteScene()
 }
 
 //! Raytracing
-// gk::Point pickLightSource()
-// {
+void pickLightSources(const int lightSampleCount, std::vector<gk::Point>& points)
+{
+  uint i;
+  int j;
 
-// }
+  gk::Point p;
 
-bool intersection(const gk::Ray& ray, const gk::Mesh& mesh, gk::Hit& hit)
+  const uint lightCount = _lights.size();
+
+  points.clear();
+
+  for (i = 0; i < lightCount; ++i)
+  {
+    for (j = 0; j < lightSampleCount; ++j)
+    {
+      _mesh->triangle(_lights[i]).sampleUniform((float)rand() / RAND_MAX, (float)rand() / RAND_MAX, p);
+
+      points.push_back(p);
+    }
+  }
+}
+
+bool intersect(const gk::Ray& ray, const gk::Mesh& mesh, gk::Hit& hit)
 {
   int i;
   int triangleCount;
 
-  float minrt;
   bool intersect;
+  gk::Hit firstHit;
 
-  minrt = -1;
   intersect = false;
+  firstHit.t = HUGE_VAL;
+
   triangleCount = mesh.triangleCount();
 
   for (i = 0; i < triangleCount; ++i)
   {
     if (mesh.triangle(i).Intersect(ray, ray.tmax, hit.t, hit.u, hit.v))
     {
-      if (minrt < 0)
-	minrt = hit.t;
-
-      if (hit.t <= minrt)
+      if (hit.t <= firstHit.t)
       {
-	minrt = hit.t;
-	hit.object_id = i;
+	firstHit = hit;
+	firstHit.object_id = i;
+
 	intersect = true;
       }
     }
   }
 
+  hit = firstHit;
+
   return intersect;
+}
+
+bool visibles(gk::Point p1, gk::Point p2)
+{
+  gk::Ray ray;
+  gk::Hit hit;
+  gk::Vector offset;
+
+  offset = gk::Normalize(p2 - p1) * RAY_EPSILON;
+
+  p1 += offset;
+  p2 -= offset;
+
+  ray = gk::Ray(p1, p2 - p1);
+  ray.tmax = 1;
+
+  return !intersect(ray, *_mesh, hit);
 }
 
 gk::Vec4 incidentLight(const gk::Point& p, const gk::Vector& d, const float tmax = HUGE_VAL)
 {
   gk::Ray ray;
+
   gk::Hit hit;
+  gk::Point hitPoint;
+
+  std::vector<gk::Point> lights;
+  const int lightSampleCount = 4;
+
+  uint l;
+  uint lightCount;
+
+  gk::Vector lightOffset;
+  gk::Point offsetHitPoint;
+  gk::Point offsetLight;
+
+  float lightingFactor;
 
   ray = gk::Ray(p + d * RAY_EPSILON, d);
   ray.tmax = tmax;
 
-  if (intersection(ray, *_mesh, hit))
-    return _triangleColors[hit.object_id];
+  if (intersect(ray, *_mesh, hit))
+  {
+    hitPoint = ray(hit.t);
+
+    pickLightSources(lightSampleCount, lights);
+
+    lightingFactor = 0;
+    lightCount = lights.size();
+
+    for (l = 0; l < lightCount; ++l)
+    {
+      lightOffset = gk::Normalize(lights[l] - hitPoint) * RAY_EPSILON;
+
+      offsetHitPoint = hitPoint + lightOffset;
+      offsetLight = lights[l] - lightOffset;
+
+      if (visibles(offsetHitPoint, offsetLight))
+	++lightingFactor;
+    }
+
+    lightingFactor /= lightCount;
+
+    return gk::Vec4(_triangleColors[hit.object_id].x * lightingFactor,
+		    _triangleColors[hit.object_id].y * lightingFactor,
+		    _triangleColors[hit.object_id].z * lightingFactor,
+		    1);
+  }
 
   return gk::Vec4(0, 0, 0, 1);
 }
@@ -125,6 +199,8 @@ int main(int, char**)
 
   gk::Point worldOrigin;
   gk::Vector worldRay;
+
+  srand(time(0));
 
   loadScene();
 
@@ -158,9 +234,9 @@ int main(int, char**)
     }
   }
 
-  gk::ImageIO::writeImage("raytracing.png", outputImage);
-  delete outputImage;
+  gk::ImageIO::writeImage("out.png", outputImage);
 
+  delete outputImage;
   deleteScene();
 
   return 0;
